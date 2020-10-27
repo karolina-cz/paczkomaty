27bf0b5dbf6c4ed5bd84cf764efec4fbef7f9d64
karolina <karolina.czachorska@gmail.com>
2020-10-27 12:04:37 +0100

    username validation; handle validation on char delete

--- static/sign-up.js
+++ static/sign-up.js
@@ -1,41 +1,52 @@
 var passwordValue = ''
-document.getElementById('photo').addEventListener('change',function(e){
-  let fileName = e.target.files[0].name;
-  let nextSibling = e.target.nextElementSibling
-  nextSibling.innerText = fileName
-  isValidFile(e.target)
+document.getElementById('photo').addEventListener('change', function (e) {
+    let fileName = e.target.files[0].name;
+    let nextSibling = e.target.nextElementSibling
+    nextSibling.innerText = fileName
+    isValidFile(e.target)
 })
 
-document.getElementById('lastname').addEventListener('change', function (e){
-  isValidName(e.target, e.target.id)
+document.getElementById('lastname').addEventListener('change', function (e) {
+    isValidName(e.target, e.target.id)
 })
 
-document.getElementById('firstname').addEventListener('change', function (e){
-  isValidName(e.target, e.target.id)
+document.getElementById('firstname').addEventListener('change', function (e) {
+    isValidName(e.target, e.target.id)
 })
 
-document.getElementById('login').addEventListener('keypress', function (e){
-  let username = e.target.value + String.fromCharCode(e.keyCode)
-  removeMessages([e.target.id]);
-  httpGetAsync( 'https://infinite-hamlet-29399.herokuapp.com/check/' + username, function(response) {
-    let responseValue = JSON.parse(response)[username]
-    if(responseValue === 'taken'){
-        addMessage(e.target,"validation-message", "Nazwa użytkownika " + e.target.value + " jest zajęta")
+document.getElementById('login').addEventListener('keydown', function (e) {
+    if (e.keyCode === 8) {
+        let username = e.target.value.slice(0, -1);
+        if(username !== ''){
+            isValidUsername(e.target, username)
+        }
     }
-  })
 })
 
-document.getElementById('repeated-password').addEventListener('change', function (e){
-  isValidPassword()
+document.getElementById('login').addEventListener('keypress', function (e) {
+    let username = e.target.value + String.fromCharCode(e.keyCode)
+    isValidUsername(e.target, username)
 })
 
-function isValidUsername(){
-    return true;
+document.getElementById('repeated-password').addEventListener('change', function (e) {
+    isValidPassword()
+})
+
+function isValidUsername(referenceNode, username) {
+    removeMessages([referenceNode.id]);
+    httpGetAsync('https://infinite-hamlet-29399.herokuapp.com/check/' + username, function (response) {
+        let responseValue = JSON.parse(response)[username]
+        if (responseValue === 'taken') {
+            addMessage(referenceNode, "validation-message", "Nazwa użytkownika " + username + " jest zajęta")
+            return false;
+        }
+        return true;
+    })
 }
 
 function httpGetAsync(theUrl, callback) {
     var xmlHttp = new XMLHttpRequest();
-    xmlHttp.onreadystatechange = function() {
+    xmlHttp.onreadystatechange = function () {
         if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
             callback(xmlHttp.responseText);
     }
@@ -43,12 +54,12 @@ function httpGetAsync(theUrl, callback) {
     xmlHttp.send(null);
 }
 
-function isValidFile(referenceNode){
+function isValidFile(referenceNode) {
     removeMessages([referenceNode.id]);
-    if(referenceNode.files[0] != null){
+    if (referenceNode.files[0] != null) {
         let fileName = referenceNode.files[0].name;
-        if(!(fileName.endsWith(".png") || fileName.endsWith(".jpg"))){
-            addMessage(referenceNode.parentNode,"validation-message",
+        if (!(fileName.endsWith(".png") || fileName.endsWith(".jpg"))) {
+            addMessage(referenceNode.parentNode, "validation-message",
                 "Plik powienien być w formacie .png lub .jpg");
             return false;
         }
@@ -56,75 +67,75 @@ function isValidFile(referenceNode){
     }
     return true;
 }
+
 //TODO add generic function for deleting messages
 //TODO send form to address
 //TODO validate username
-function isValidName (referenceNode, referenceNodeId){
+function isValidName(referenceNode, referenceNodeId) {
     removeMessages([referenceNodeId]);
     let name = referenceNode.value;
-    if(!containsLettersOnly(name)){
-        addMessage(referenceNode,"validation-message", "To pole powinno zawierać tylko litery")
+    if (!containsLettersOnly(name)) {
+        addMessage(referenceNode, "validation-message", "To pole powinno zawierać tylko litery")
         return false;
     }
-    if(name === ''){
-        addMessage(referenceNode,"validation-message", "To pole musi być uzupełnione")
+    if (name === '') {
+        addMessage(referenceNode, "validation-message", "To pole musi być uzupełnione")
         return false;
     }
     return true;
 }
 
-function removeMessages(referenceNodesIds){
+function removeMessages(referenceNodesIds) {
     referenceNodesIds.forEach(id => {
         let element = document.getElementById(id + "-msg");
-        if(element != null){
+        if (element != null) {
             element.parentNode.removeChild(element);
         }
     })
 }
 
-function containsLettersOnly(text){
+function containsLettersOnly(text) {
     return !/[^a-zA-Z]/.test(text);
 }
 
-function addMessage(referenceNode, messageType, message){
+function addMessage(referenceNode, messageType, message) {
     let tag = document.createElement("p");
     let text = document.createTextNode(message);
-   tag.appendChild(text);
-   tag.id = referenceNode.id + '-msg'
-   tag.classList.add(messageType);
-   insertAfter(referenceNode, tag);
+    tag.appendChild(text);
+    tag.id = referenceNode.id + '-msg'
+    tag.classList.add(messageType);
+    insertAfter(referenceNode, tag);
 }
 
 function insertAfter(referenceNode, newNode) {
-  referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+    referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
 }
 
-function isValidPassword (){
+function isValidPassword() {
     let password = document.getElementById("password");
     let repeatedPassword = document.getElementById("repeated-password");
     removeMessages([password.id, repeatedPassword.id]);
-    if(password.value === '' && repeatedPassword.value === ''){
+    if (password.value === '' && repeatedPassword.value === '') {
         addMessage(password, "validation-message", "To pole musi być uzupełnione");
         addMessage(repeatedPassword, "validation-message", "To pole musi być uzupełnione");
         return false;
-    }
-    else if(password.value !== repeatedPassword.value){
+    } else if (password.value !== repeatedPassword.value) {
         addMessage(repeatedPassword, "validation-message", "Hasła muszą być takie same");
         return false;
     }
     return true;
 }
 
-function validateForm(){
+function validateForm() {
     let firstname = document.getElementById("firstname");
     let surname = document.getElementById("surname");
     let fileInput = document.getElementById("image");
-    let isNameValid = isValidName(firstname,firstname.id);
+    let isNameValid = isValidName(firstname, firstname.id);
     let isSurnameValid = isValidName(surname, surname.id);
     let isPasswordValid = isValidPassword();
     let isUsernameValid = isValidUsername();
     let isFileValid = isValidFile(fileInput);
-    if(!(isNameValid && isSurnameValid && isPasswordValid && isUsernameValid && isFileValid)){
+    if (!(isNameValid && isSurnameValid && isPasswordValid && isUsernameValid && isFileValid)) {
         console.log('something wrong');
         event.preventDefault();
     }
